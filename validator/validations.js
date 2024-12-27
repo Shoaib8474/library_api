f52204c47e0fa41d1ccc9baabb25f76254a3688e
Shoaib Alom <131032802+Shoaib8474@users.noreply.github.com>
2024-12-27 11:09:53 +0530

    Update validations.js

--- validator/validations.js
+++ validator/validations.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 // Schema for user registration
 const registerSchema = Joi.object({
-  username: Joi.string().min(3).max(30).required(),
+  // username: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
 });
